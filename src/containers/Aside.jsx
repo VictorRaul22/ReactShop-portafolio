c6e6ef8452d90d75583974e02eaff9631aebfbd0
victorraul <victor-raul-22@hotmail.com>
2022-03-04 13:37:54 -0500

    fix aside scroll with respect to Layout

--- src/containers/Aside.jsx
+++ src/containers/Aside.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import "@styles/Aside.scss"
-function Aside({children,toggleOrders}) {
-  
-  return (
-    <aside className={`Aside`}>
-      {children}
-    </aside>
-  )
+import React from "react";
+import reactDom from "react-dom";
+import "@styles/Aside.scss";
+function Aside({ children, toggleOrders }) {
+  console.log(toggleOrders);
+
+  return <aside className={`Aside`}>{children}</aside>;
 }
 
-export {Aside}
+export { Aside };
--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import "@styles/Layout.scss"
-import {Header} from "@organismo/Header"
-function Layout({children}) {
+import React, { useContext } from "react";
+import "@styles/Layout.scss";
+import { Header } from "@organismo/Header";
+import AppContext from "@context/AppContext";
+function Layout({ children }) {
+  const { toggleOrders } = useContext(AppContext);
   return (
-    <div className='Layout'>
-      <Header/> 
+    <div className={`Layout ${toggleOrders ? "Layout-hidden-scroll" : ""}`}>
+      <Header />
       {children}
     </div>
-  )
+  );
 }
 
-export {Layout}
+export { Layout };
